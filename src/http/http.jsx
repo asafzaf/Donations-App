ca944b2f9911f0547ff5d5e89b1da13ac208fd5e
asafzaf <asaf.zafrir@gmail.com>
2024-02-27 18:02:50 +0200

    added all functions.

--- src/http/http.jsx
+++ src/http/http.jsx
@@ -10,7 +10,6 @@ export const http = axios.create({
 const getDonations = async () => {
     const response = await http.get("/api/donations/records");
     return response.data.data.donations;
-
 };
 
 const getDonationById = async (id) => {
@@ -18,4 +17,19 @@ const getDonationById = async (id) => {
     return response.data.data.donation;
 }
 
-export {getDonations, getDonationById};
+const createDonation = async (donation) => {
+    const response = await http.post("/api/donations/items", donation);
+    return response.data.data.donation;
+}
+
+const updateDonation = async (id, donation) => {
+    const response = await http.put(`/api/donations/items/${id}`, donation);
+    return response.data.data.donation;
+}
+
+const deleteDonation = async (id) => {
+    const response = await http.delete(`/api/donations/items/${id}`);
+    return response.data.data.donation;
+}
+
+export {getDonations, getDonationById, createDonation, updateDonation, deleteDonation};
