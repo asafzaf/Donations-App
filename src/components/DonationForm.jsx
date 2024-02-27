c13ed96c4ce8446dd415d14dd53caf6ad982560d
asafzaf <asaf.zafrir@gmail.com>
2024-02-27 16:15:06 +0200

    created donation form for edit or creation use

---
+++ src/components/DonationForm.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { Grid, TextField } from "@mui/material";
+import "../styles.css";
+
+const DonationForm = (props) => {
+  const [name, setName] = useState(
+    props.donation?.name ? props.donation?.name : ""
+  );
+  const [email, setEmail] = useState(
+    props.donation?.email ? props.donation?.email : ""
+  );
+  const [amount, setAmount] = useState(
+    props.donation?.amount ? props.donation?.amount : ""
+  );
+
+  const date = props.donation?.date
+    ? new Date(props.donation?.date)
+        .toUTCString("en-US")
+        .slice(0, 16)
+        .toString()
+    : new Date().toUTCString("en-US").slice(0, 16).toString();
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} md={6} className="field">
+        <p>
+          <b>Donors Name:</b>
+        </p>
+      </Grid>
+      <Grid item xs={12} md={6} className="field">
+        <TextField
+          name="name"
+          datatype="text"
+          label="Donors Name"
+          placeholder="John Doe"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        ></TextField>
+      </Grid>
+      <Grid item xs={12} md={6} className="field">
+        <p>
+          <b>E-mail:</b>
+        </p>
+      </Grid>
+      <Grid item xs={12} md={6} className="field">
+        <TextField
+          datatype="email"
+          label="E-mail"
+          placeholder="John@Doe.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        ></TextField>
+      </Grid>
+      <Grid item xs={12} md={6} className="field">
+        <p>
+          <b>Amount:</b>
+        </p>
+      </Grid>
+      <Grid item xs={12} md={6} className="field">
+        <TextField
+          datatype="number"
+          label="Amount"
+          placeholder="1000"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          required
+        ></TextField>
+      </Grid>
+      <Grid item xs={12} className="field">
+        <p>
+          <b>Date:</b> {date}
+        </p>
+      </Grid>
+    </Grid>
+  );
+};
+DonationForm.propTypes = {
+  props: PropTypes.object,
+  donation: PropTypes.object,
+};
+
+export default DonationForm;
