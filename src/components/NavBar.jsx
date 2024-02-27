d8dd37133900c397e2d1953b9138f9f275e02565
asafzaf <asaf.zafrir@gmail.com>
2024-02-27 16:13:28 +0200

    fixed the nav bar for better expirence

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,41 @@
 import "../styles.css";
-import { Box } from "@mui/material";
+import { useState } from "react";
+import { Box, Input } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <Box className="navBar">
-            <Box className="navItem">Show All Donations</Box>
-            <Box className="navItem">Show Donation by id</Box>
-            <Box className="navItem">create a new donation</Box>
-            <Box className="navItem">update a donation</Box>
-            <Box className="navItem">delete a donation</Box>
-        </Box>
-    );
-    }
-    export default NavBar;
+  const [serachString, setSearchString] = useState("");
+
+  return (
+    <Box className="navBar">
+      <NavLink component="button" to="/" style={{ textDecoration: "none" }}>
+        <Box className="navItem">Show All Donations</Box>
+      </NavLink>
+      <NavLink
+        component="button"
+        to="/create"
+        style={{ textDecoration: "none" }}
+      >
+        <Box className="navItem">Create a new donation</Box>
+      </NavLink>
+      <Box className="searchBar">
+        <Input
+          style={{
+            width: "100%",
+            color: "white",
+            hover: "white",
+            focus: "white",
+          }}
+          type="text"
+          disableUnderline={true}
+          placeholder="Search by id..."
+          onChange={(e) => setSearchString(e.target.value)}
+        />
+      </Box>
+      <NavLink content="button" to={`/${serachString}`}  state={{ id: serachString }} style={{ textDecoration: "none" }}>
+        <Box className="navItem">Search</Box>
+      </NavLink>
+    </Box>
+  );
+};
+export default NavBar;
