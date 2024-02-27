2c1e3adb8e9e434ba2f59414995be7260b775b19
asafzaf <asaf.zafrir@gmail.com>
2024-02-27 19:20:28 +0200

    added spinner. added delete handler - pass it into button.

--- src/screens/DonationItemScreen.jsx
+++ src/screens/DonationItemScreen.jsx
@@ -8,6 +8,8 @@ import Title from "../components/Title";
 import "../styles.css";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { deleteDonation } from "../http/http";
+import { PacmanLoader } from "react-spinners";
 
 const DonationItemScreen = () => {
   const [item, setItem] = useState({});
@@ -26,34 +28,54 @@ const DonationItemScreen = () => {
     setIsLoading(false);
   }, [id]);
 
+  const deleteHandler = () => {
+    setIsLoading(true);
+    console.log("Delete");
+    deleteDonation(id)
+      .then(() => {
+        const path = "/";
+        setTimeout(() => {
+          window.location.href = path;
+        });
+      }, 3000)
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div>
       <Header />
       <NavBar />
       <Title title="Search Results" />
       <Box className="container">
-        <p>{isLoading ? "Loading..." : null}</p>
-        <Box className="item">
-          <Box className="sideButtons">
-            <NavLink to={`/edit/${id}`} style={{textDecoration: 'none', color: 'black'}}>
-              <EditIcon className="iconButton" />
-            </NavLink>
-            <DeleteIcon className="iconButton" />
+        {isLoading ? (
+          <PacmanLoader color="#2D9596" />
+        ) : (
+          <Box className="item">
+            <Box className="sideButtons">
+              <NavLink
+                to={`/edit/${id}`}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <EditIcon className="iconButton" />
+              </NavLink>
+
+              <DeleteIcon className="iconButton" onClick={deleteHandler} />
+            </Box>
+            <h3>
+              <b>Donors Name:</b> {item.name}
+            </h3>
+            <p>
+              <b>E-mail:</b> {item.email}
+            </p>
+            <p>
+              <b>Amount:</b> {item.amount}
+            </p>
+            <p>
+              <b>Date:</b>{" "}
+              {new Date(item.date).toUTCString("en-US").slice(0, 16).toString()}
+            </p>
           </Box>
-          <h3>
-            <b>Donors Name:</b> {item.name}
-          </h3>
-          <p>
-            <b>E-mail:</b> {item.email}
-          </p>
-          <p>
-            <b>Amount:</b> {item.amount}
-          </p>
-          <p>
-            <b>Date:</b>{" "}
-            {new Date(item.date).toUTCString("en-US").slice(0, 16).toString()}
-          </p>
-        </Box>
+        )}
       </Box>
     </div>
   );
