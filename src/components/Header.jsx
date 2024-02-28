f971109d198259ced3ac54d9e1d6f70e3da5da61
asafzaf <asaf.zafrir@gmail.com>
2024-02-28 20:42:27 +0200

    change title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Box } from "@mui/material";
 const Header = () => {
   return (
     <Box className="header">
-      <h1 className="headerTitle">Assignment 3</h1>
+      <h1 className="headerTitle">Donations Express & React</h1>
     </Box>
   );
 };
