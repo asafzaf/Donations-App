1fa860e01a072122752aa3e99deb8a21df93509b
asafzaf <asaf.zafrir@gmail.com>
2024-02-29 20:28:14 +0200

    new 404 page

---
+++ src/screens/NotFoundScreen.jsx
@@ -0,0 +1,23 @@
+import { Box, Button } from "@mui/material";
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+import Title from "../components/Title";
+
+const NotFoundScreen = () => {
+    return (
+        <div>
+          <Header />
+          <NavBar />
+          <Title title="All Donations" />
+          <Box className="container">
+            <Box className="item">
+                <h3>Oh no... You lost the way (404)</h3>
+                <Button href="/" variant="contained">Go back home</Button>
+            </Box>
+          </Box>
+        </div>
+      );
+
+}
+
+export default NotFoundScreen;
